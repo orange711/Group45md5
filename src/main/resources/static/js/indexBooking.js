88cb34eba31e0dffdfaf73ee50d147379f0a74d9
lyh-cs <18810988550@163.com>
2021-10-16 22:04:09 +0800

    fix booking problems with input
    fix booking problems with input

--- src/main/resources/static/js/indexBooking.js
+++ src/main/resources/static/js/indexBooking.js
@@ -15,30 +15,39 @@ function check(t){
     vaccineId = $($("select[name='brand'] option:selected")[t]).val();
     date = $($("input[name='date']")[t]).val();
     bookingTimezone = $($("input[name='time']")[t]).val();
-    $("#confirm_provider").text(provider);
-    $("#confirm_brand").text(brand);
-    $("#confirm_date").text(date+' '+bookingTimezone);
-    $("#confirm_provider1").text(provider);
-    $("#confirm_brand1").text(brand);
-    $("#confirm_date1").text(date+' '+bookingTimezone);
-    let tt = $("#confirm_content").html();
-    layer.open({
-        type: 0 , //选择默认Layer 0（信息框，默认）
-        title: 'CONFIRM YOUR BOOKING',
-        shade: 0.4,
-        content: tt , //获取DOM元素
-        btn: ['Confirm', 'Cancel'], //按钮组
-        scrollbar: false , //屏蔽浏览器滚动条
-        yes: function(index){    //点击确定回调
-            layer.close(index);
-            saveBooking();     //调用保存方法
-            reduceVaccine(vaccineId);
-        },
-        btn2: function(){      // 点击Cancel回调
-            layer.closeAll();
-        }
-    })
-    // }
+    if(!vaccineId|| !date|| !bookingTimezone){
+        layer.confirm('Please choose your prefer vaccine and booking time.', {
+            btn: ["OK"],
+            icon: 2,
+            title: "Booking check!"
+        }, function () {
+            //点击确后关闭提示框
+            layer.closeAll('dialog');
+        });
+    }else{
+        $("#confirm_provider").text(provider);
+        $("#confirm_brand").text(brand);
+        $("#confirm_date").text(date+' '+bookingTimezone);
+        $("#confirm_provider1").text(provider);
+        $("#confirm_brand1").text(brand);
+        $("#confirm_date1").text(date+' '+bookingTimezone);
+        let tt = $("#confirm_content").html();
+        layer.open({
+            type: 0 ,//选择默认Layer 0（信息框，默认）
+            title: 'CONFIRM YOUR BOOKING',
+            shade: 0.4,
+            content: tt ,//支持获取DOM元素
+            btn: ['Confirm', 'Cancel'], //设定按钮
+            scrollbar: false ,
+            yes: function(index){    //点击确定回调
+                layer.close(index);
+                saveBooking();//调用保存方法
+            },
+            btn2: function(){// 点击Cancel回调
+                layer.closeAll();
+            }
+        })
+    }
 }
 function reduceVaccine(vaccineId){
     var data = {
