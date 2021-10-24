ea6ae3c4458a95b1ff3eaa0da102a569983070ba
wsn976654769 <976654769@qq.com>
2021-10-25 00:20:28 +1100

    refactor

--- target/classes/static/js/userLogin.js
+++ target/classes/static/js/userLogin.js
@@ -1,11 +1,9 @@
 function login() {
-    //获取用户名和密码
     const name = $("#account").val();//输入的用户名
     const password = $("#password").val();//输入的密码
     console.log(name);
     console.log(password)
     var data = {userAccount: name, userPassword: password}
-    //调ajax
     $.ajax({
         url: "http://localhost:8080/vacBook/user/login",
         data: data,
@@ -16,7 +14,7 @@ function login() {
             if(data === "true"){
                 console.log(data + "200");
                 layer.msg("Login success!" );
-                setTimeout(function () {//两秒后跳转
+                setTimeout(function () {
                     location.href = "index/booking";
                 }, 2000);
             }else{
