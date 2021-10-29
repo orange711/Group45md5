cd8e33d71a84674aeecb6b6dc801e6a1722ffb8d
wsn976654769 <976654769@qq.com>
2021-10-29 19:55:17 +1100

    add admin unit test

--- src/test/java/com/sydney/vacbook/AdminControllerTest.java
+++
@@ -1,4 +0,0 @@
-package com.sydney.vacbook;
-
-public class AdminControllerTest {
-}
--- src/test/java/com/sydney/vacbook/VaccineUnitTest.java
+++ src/test/java/com/sydney/vacbook/VaccineUnitTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
-class VaccineControllerTest {
+class VaccineUnitTest {
     @Autowired
 
     private VaccineController vaccineController;
